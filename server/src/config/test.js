b122bdea50b49d3e9e5a9c533730cc5bbf70c604
muhadel <mohamed.adel@barqsystems.com>
2020-10-31 18:46:00 +0200

    remove test.js

--- server/src/config/test.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-// require('dotenv').config({ path: path.join(__dirname, '../../.env') });
-console.log(path.join(__dirname, '../../.env'));
-console.log(__dirname + '../../.env');
-
-// require('dotenv').config({ path: __dirname + '../.env' });
